3571bee30f39cc917cebb27e8b5392a71f32f228
shantelletoh <shantelleschool@gmail.com>
2022-06-26 16:17:56 -0700

    connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})  // connect to database
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error));
